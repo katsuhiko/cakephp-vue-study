d1172d8ed798bdfc959929bcc41da4a8fdf05554
nagashima <katsuhiko.nagashima@gmail.com>
2020-03-24 22:54:22 +0900

    docs: 少し修正しました。

--- src/Controller/Api/swagger.php
+++ src/Controller/Api/swagger.php
@@ -2,11 +2,8 @@
 
 /**
  * @OA\Info(
- *  title="CakePHP Vue Study",
- *  description="CakePHP Vue Study",
- *  version="1.0.0",
- *   @OA\Contact(
- *     email="katsuhiko.nagashima@gmail.com"
- *   )
+ *   title="CakePHP Vue Study",
+ *   description="CakePHP Vue Study API List",
+ *   version="1.0.0",
  * )
  */
